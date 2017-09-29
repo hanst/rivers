9107897df3c68dc2168939d2b9862f3061e08913
fnOpenSource <32325515+fnOpenSource@users.noreply.github.com>
2017-09-29 09:49:40 +0800

    Delete start.sh

--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#OPTS
-JAVA_OPTS="$JAVA_OPTS -server -Xms2g -Xmx2g -Xmn512M -XX:+UseConcMarkSweepGC -XX:ParallelGCThreads=8 -XX:+HeapDumpOnOutOfMemoryError"
-
-nohup ${JAVA_HOME}/bin/java ${JAVA_OPTS} -jar  river.jar &
